69c8c634184c92c2a8c7d909c8229bd8876a5291
LieuHongPhuoc <108378081+LieuHongPhuoc@users.noreply.github.com>
2024-07-16 10:30:33 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clusterlhp.s2inlyk.mongodb.net',
+    DB_USER: 'phuocLH',
+    DB_PASS: 'Taolao230702002',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'LHP',
+    JWT_EXPIRES: '3600000', // in milliseconds
+    EMAIL_USER: 'phuocphuphan20022@gmail.com', // gmail service
+    EMAIL_PASS: 'iseu wign dizx ikpk'
+  };
+  module.exports = MyConstants;
